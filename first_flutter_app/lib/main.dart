a54a0c81707676d35e5828ab0eb77be9138169b7
mynameisdaun <daun9870.jung@gmail.com>
2021-12-14 01:02:39 +0900

    init

---
+++ first_flutter_app/lib/main.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _MyApp();
+  }
+}
+
+class _MyApp extends State<MyApp> {
+  var switchValue = false;
+  String test = 'hello';
+  Color _color = Colors.blue;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        darkTheme: ThemeData.light(),
+        home: Scaffold(
+            body: Center(
+                child: ElevatedButton(
+                  child: Text('$test'),
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(_color)
+                  ),
+                  onPressed: () {
+                    if(_color == Colors.blue) {
+                      setState(() {
+                        _color = Colors.red;
+                      });
+                    } else {
+                      setState(() {
+                        _color = Colors.blue;
+                      });
+                    }
+                  })),
+                ),
+            );
+  }
+}
